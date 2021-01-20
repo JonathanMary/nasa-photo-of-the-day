afe8d17d60a32fa0be62c0b41d366851b0a4dfed
JonathanMary <jon85ma@gmail.com>
2021-01-20 22:51:46 +0100

    MVP DONE, No CSS, mind your eyes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+import { BASE_URL, API_KEY } from "./constants";
+
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
+         .then(res => {
+           //console.log(res.data);
+           setData(res.data);
+         })
+         .catch(err => console.log(err))
+  },[]);
+
+  console.log(data);
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className="App container">
+      <header>
+        <h1>{data["title"]}</h1>
+      </header>
+      <section>
+        <img src={data.url} alt="astronomy"></img>
+      <p>{data.explanation}</p>
+      <p>{data.date}</p>
+      </section>
+      <footer>
+        <p>Copyright &copy; 2021 All Rights Reserved by <a href="https://api.nasa.gov">Nasa</a></p>
+      </footer>
+      
     </div>
   );
 }
